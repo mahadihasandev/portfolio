41afe6c001199f7b3f8e26082be6cbd7b781342d
mahadihasandev <mayhaydihasan.com@gmail.com>
2026-03-11 11:25:44 +0600

    fix Actionbutton cv link

--- src/components/hero/ActionButtons.tsx
+++ src/components/hero/ActionButtons.tsx
@@ -6,7 +6,7 @@ export function ActionButtons() {
   return (
     <div className="flex flex-wrap gap-4 justify-center">
       <a
-        href="https://drive.google.com/file/d/1WB4_7B3KYrullI1Et8mEVJjhwh_1GbwI/view?usp=drive_open"
+        href="https://drive.google.com/file/d/1_iLHTkWCbSnsWklzhyacOUafRP8U88Te/view"
         target="_blank" rel="noopener noreferrer"
         className="flex items-center gap-2 px-3 py-2 rounded-md text-sm sm:text-base bg-blue-600 text-white hover:bg-blue-700 transition-transform duration-300 transform hover:scale-105"
       >
