92dd5354bf1bf5fa580f7545dbc484278ad17eee
mahadihasandev <mayhaydihasan.com@gmail.com>
2026-03-31 21:56:42 +0600

    set themes to light every relode themes set to dark by deafult

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import App from './App.tsx';
 import './index.css';
 
 // Apply initial theme before render to prevent flash
-const savedTheme = localStorage.getItem('theme');
-const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-const isDark = savedTheme ? savedTheme === 'light' : prefersDark;
+localStorage.setItem('theme', 'light');
+document.documentElement.classList.remove('dark');
+// const savedTheme = localStorage.getItem('theme');
+// const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+// const isDark = savedTheme ? savedTheme === 'dark' : prefersDark;
 
-if (isDark) {
-  document.documentElement.classList.add('dark');
-}
+// if (isDark) {
+//   document.documentElement.classList.add('dark');
+// }
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
